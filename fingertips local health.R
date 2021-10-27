1288353fbb27d3aed6b5b027b16659ee4fda6051
Shhh <shanwilkinson2@users.noreply.github.com>
2021-10-27 18:00:19 +0100

    split files, import all enngland msoas

--- fingertips local health.R
+++ fingertips local health.R
@@ -7,108 +7,15 @@ install.packages("miniUI")
 #################### load packages ################################
 
 library(dplyr)
-library(shinycssloaders)
+# library(shinycssloaders)
 library(fingertipsR)
-library(data.table)
+#library(data.table)
 #library(nomisr)
-library(readxl)
-library(sf)
-library(leaflet)
-library(leaflet.extras)
+#library(readxl)
+# library(sf)
+# library(leaflet)
+# library(leaflet.extras)
 
-#################### read in datasets created below ###############
-
-# ensure working directory is the upper folder neighbourhood_info
-
-# boundaries
-  msoa_boundaries <- readRDS("msoa boundaries.RDS")
-  neighbourhood_boundaries <- readRDS("./bolton_neighbourhoods/neighbourhood boundaries.RDS") # in the app folder
-
-# data
-  # neighbourhood_data <- readRDS("./bolton_neighbourhoods/neighbourhood_indicators.RDS")
-  #local_health_data_msoa <- readRDS("./bolton_neighbourhoods/local health data with boundaries.RDS") # in the app folder
-  #neighbourhood_indicators <- readRDS("./bolton_neighbourhoods/dashboard_indicators.RDS")
-  
-  # hopefully single dataset to replace the 2 above
-  neighbourhood_indicators <- readRDS("./bolton_neighbourhoods/neighbourhood_indicators.RDS") # in the app folder
-
-  # lookups
-    lsoa_neighbourhood <- readRDS("lsoa_neighbourhood.rds")
-    #msoa_neighbourhood <- readRDS("msoas_neighbourhood.rds")
-    msoa_neighbourhood_multiple <- fread("msoas_neighbourhood_multiple.csv")
-    
-    saveRDS(msoa_neighbourhood_multiple, "./bolton_neighbourhoods/msoa_neighbourhood_multiple.RDS") # in the app folder
-    
-###################### boundaries ################################
-
-# # Bolton boundary - not actually using this
-# # Bolton boundary clipped to 20m https://geoportal.statistics.gov.uk/datasets/local-authority-districts-december-2019-boundaries-uk-bgc
-# la_boundary <- st_read("https://opendata.arcgis.com/datasets/0e07a8196454415eab18c40a54dfbbef_0.geojson") %>%
-#   filter(lad19nm == "Bolton") %>%
-#   st_set_crs(4326) # lat long
-
-
-# LSOA boundaries & turning them into neighbourhood boundaries
-# LSOA boundaries 2011 (current)
-# https://geoportal.statistics.gov.uk/datasets/lower-layer-super-output-areas-december-2011-generalised-clipped-boundaries-in-england-and-wales  
-  lsoas_2011 <- st_read("G:\\Mapping Data\\R\\map\\Lower_Layer_Super_Output_Areas_December_2011_Generalised_Clipped__Boundaries_in_England_and_Wales/Lower_Layer_Super_Output_Areas_December_2011_Generalised_Clipped__Boundaries_in_England_and_Wales.shp")
-
-# add boroughs variable from LSOA name
-  lsoas_2011 <- lsoas_2011 %>%
-    mutate(borough = stringr::str_sub(lsoa11nm, 1, nchar(as.character(lsoa11nm))-5)) %>%
-    st_transform(crs = 4326) # transforms to lat/ long from OSGB36
-
-# filter lsoas 2011 Bolton only
-  lsoas_bolton <- filter(lsoas_2011, borough %in% "Bolton")
-# plot(st_geometry(lsoas_bolton)) # check areas look right  
-  rm(lsoas_2011) # remove whole country of lsoas
-
-# get lsoa-neighbourhood link (off Boltonjsna.org.uk)
-# includes value for Bolton to keep whole borough value
-  lsoa_neighbourhood <- readRDS("lsoa_neighbourhood.rds")
-
-# add in neighbourhoood name to lsoa boundaries
-  lsoas_bolton2 <- left_join(lsoas_bolton %>%
-                               select(lsoa11cd, lsoa11nm),
-                             lsoa_neighbourhood %>%
-                               select(neighbourhood_name, lsoa_name),
-                             by = c("lsoa11cd" = "lsoa_name"))
-
-# get just outer neighbourhood boundary
-  neighbourhood_boundaries <- lsoas_bolton2 %>%
-    select(neighbourhood_name) %>%
-    group_by(neighbourhood_name) %>%
-    summarise(geometry = st_union(geometry)) %>%
-    ungroup()
-
-# save as outer neighbourhood boundaries
-  saveRDS(neighbourhood_boundaries, "neighbourhood boundaries.RDS")
-
-
-# MSOA boundaries
-# https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-boundaries-bgc
-  msoas_2011 <- st_read("G:\\Mapping Data\\R\\map\\MSOA/Middle_Layer_Super_Output_Areas_December_2011_Boundaries_BGC.shp")
-
-# add borough variable from MSOA name
-  msoas_2011 <- msoas_2011 %>%
-    mutate(borough = stringr::str_sub(msoa11nm, 1, nchar(as.character(msoa11nm))-4)) %>%
-    st_transform(crs = 4326) # transforms to lat/ long from OSGB36
-
-# filter msoas 2011 Bolton only
-  msoas_bolton <- filter(msoas_2011, borough %in% "Bolton")
-# plot(st_geometry(msoas_bolton)) # check areas look right  
-  rm(msoas_2011) # remove whole country of lsoas
-
-# join in neighbourhood name & all msoa names lookup 
-  msoa_neighbourhood <- readRDS("msoas_neighbourhood.rds")
-
-  msoas_bolton2 <- msoas_bolton %>%
-    select(msoa11cd) %>%
-    left_join(msoa_neighbourhood,
-              by = c("msoa11cd" = "msoa_code"))
-
-# save as neighbourhood boundaries with all msoa names & neighbourhood
-  saveRDS(msoas_bolton2, "msoa boundaries.RDS")
 
 ################# get data from phe fingertips local health #########################  
   
@@ -130,18 +37,30 @@ library(leaflet.extras)
       "E02001013", "E02001014", "E02001015", "E02001016", "E02001017",
       "E02001018")
 
-  # get msoa local health data
-    local_health_msoa <- fingertips_data(IndicatorID = local_health_indicators$IndicatorID,
-                                        ProfileID = 143,
-                                        AreaTypeID = 3, # 3 = MSOA
-                                        AreaCode = bolton_area_codes
-                                        ) 
+  # # get msoa local health data
+  #   local_health_msoa <- fingertips_data(IndicatorID = local_health_indicators$IndicatorID,
+  #                                       ProfileID = 143,
+  #                                       AreaTypeID = 3, # 3 = MSOA
+  #                                       AreaCode = bolton_area_codes
+  #                                       )
+    
   # get borough local health data
     local_health_borough <- fingertips_data(IndicatorID = local_health_indicators$IndicatorID,
                                        ProfileID = 143,
                                        AreaTypeID = 402, # 402 = UTLA with boundary changes post Apr 2021
                                        AreaCode = "E08000001" # bolton
                                         ) 
+    
+    # get all msoas local health data - takes a bit of a while
+    local_health_all_msoa <- fingertips_data(IndicatorID = local_health_indicators$IndicatorID,
+                                         ProfileID = 143,
+                                         AreaTypeID = 3#, # 3 = MSOA
+                                         # AreaCode = bolton_area_codes
+                                          ) 
+    
+    # filter just bolton
+    local_health_msoa <- local_health_all_msoa %>%
+      filter(AreaCode %in% bolton_area_codes)
 
   # join msoa & borough data
     local_health <- bind_rows(local_health_msoa, local_health_borough)
@@ -154,24 +73,8 @@ library(leaflet.extras)
 #   msoa_neighbourhood <- readRDS("msoas_neighbourhood.RDS")
 # includes value for Bolton to keep whole borough value
   # version where MSOAs appear in more than 1 neighbourhood
-  msoa_neighbourhood_multiple <- fread("msoas_neighbourhood_multiple.csv")
+  msoa_neighbourhood_multiple <- data.table::fread("msoas_neighbourhood_multiple.csv")
 
-  # # add in neighbourhood
-  #   bolton_local_health2 <- left_join(local_health, msoa_neighbourhood, 
-  #                                     by = c("AreaName"= "msoa_name")) %>%
-  #     # keep latest value only - only seems to include latest anyway
-  #     group_by(IndicatorID, Sex, Age, AreaName) %>%
-  #     filter(TimeperiodSortable == max(TimeperiodSortable)) %>%
-  #     ungroup() %>%
-  #     # add in domain ie part of the profile 
-  #     left_join(local_health_indicators %>% 
-  #                 select(IndicatorID, DomainID, DomainName, ProfileID, ProfileName),
-  #               by = "IndicatorID") %>%
-  #     filter(ProfileID == 143) %>% # local health profile = 143, some indicators are in multiple
-  #     arrange(ProfileID, DomainID) %>%
-  #     group_by(IndicatorID, Sex, Age, neighbourhood)
-
-    
     # add in neighbourhood using multiple file
     bolton_local_health2 <- full_join(local_health, msoa_neighbourhood_multiple, 
                                       by = c("AreaName"= "msoa_name")) %>%
@@ -272,128 +175,11 @@ library(leaflet.extras)
                                       select(msoa11cd), # only want the join field & geometry whcih sticks anyway
                                       nbourhood_indicators2, # right join to keep geometry
                by = c("msoa11cd" = "msoa_code")
-  )
-  # ) %>%
-  #   right_join(# right join to keep geometry
-  #              bolton_local_health2,
-  #              by = c("msoa11cd" = "AreaCode")
-  #   )
+                 )
+
   
-# save fo app
+# save for app
   saveRDS(nbourhood_indicators3, "./bolton_neighbourhoods/neighbourhood_indicators.RDS")
-
-
-##################### map ###############################
-
-mytitle <- glue::glue("<b>Title</b><br>
-                      Subtitle | Turn layers on & off to explore")
-
-lsoa_labels <- (glue::glue("<b>LSOA</b><br>
-                    Code: {lsoas_bolton$lsoa11cd}<br>
-                    Name: {lsoas_bolton$lsoa11nmw} <br>
-                    All age population: {lsoas_bolton$all_age_pop}"))
-
-msoa_labels = (glue::glue("<b>MSOA</b><br>
-                    Code: {msoas_bolton$msoa11cd}<br>
-                    Name: {msoas_bolton$msoa11nmw}<br>
-                    House of Commons Library name: {msoas_bolton$local_name}<br>
-                    Covid cases rolling rate: {msoas_bolton$new_cases_by_specimen_date_rolling_rate}<br>
-                    Covid cases date: {format(msoas_bolton$date, '%d/%m/%Y')}"))
-
-# make colour palatte 
-msoa_colours <- colorNumeric(
-  palette = "Greens", 
-  domain = msoas_bolton$new_cases_by_specimen_date_rolling_rate, 
-  na.color = "white")
-
-leaflet() %>%
-  addResetMapButton() %>%
-  addProviderTiles("Stamen.TonerLite") %>%
-  addPolylines(data = neighbourhood_boundaries, weight = 4, color = "black") %>%
-  addPolygons(data = msoas_bolton, weight = 0.75, color = "grey", 
-              fillColor = ~msoa_cases_colours(new_cases_by_specimen_date_rolling_rate), fillOpacity = 0.5, group = "MSOA cases",
-              highlight = highlightOptions(weight = 4, color = "grey", bringToFront = TRUE),
-              popup = ~msoa_labels, 
-              labelOptions = labelOptions(
-                style = list("font-weight" = "normal", padding = "3px 8px"),
-                textsize = "15px",
-                direction = "auto"))
-
-###################### chart ##########################
-
-library(plotly)
-
-neighbourhood_indicators <- readRDS("./bolton_neighbourhoods/neighbourhood_indicators.RDS") # in the app folder
-
-neighbourhood_name <- "Turton"
-neighbourhood_name <- "Central/Great Lever"
-neighbourhood_name <- "Chorley Roads"
-
-indicator_name <- "Percentage of the total resident population who are 65 and over"
-
-chart_data <- neighbourhood_indicators %>%
-  st_drop_geometry() %>%
-  filter(neighbourhood == neighbourhood_name &   
-           IndicatorName == indicator_name) %>%
-  #group_by(neighbourhood) %>%
-  slice(1) 
-
-  # chart
-  plot_ly() %>%
-    add_trace(type = "box",
-              y =1,
-              q1 = chart_data$nbourhood_min,
-              # calculated value if it's available otherwise median
-              median = ifelse(is.na(chart_data$nbourhood_pct), chart_data$nbourhood_median,chart_data$nbourhood_pct),
-              q3 = chart_data$nbourhood_max,
-              lowerfence = chart_data$bolton_min,
-              upperfence = chart_data$bolton_max,
-              notchspan = 0.3
-              )
-  
-  # chart
-  plot_ly() %>%
-    add_trace(type = "box",
-              y = list("Bolton", "Neighbourhood"),
-              q1 = list(chart_data$bolton_min, chart_data$nbourhood_min),
-              # calculated value if it's available otherwise median
-              median = list(chart_data$bolton_value,
-                ifelse(is.na(chart_data$nbourhood_pct), 
-                                   chart_data$nbourhood_median,
-                                   chart_data$nbourhood_pct)
-                            ),
-              q3 = list(chart_data$bolton_max, chart_data$nbourhood_max),
-             #min = list(chart_data$bolton_min, chart_data$nbourhood_min),
-            #  upperfence = chart_data$bolton_max,
-              notchspan = list(0.3, 0.3),
-            #hovertemplate = "some text {chart_data$bolton_max}",
-           hovertext = ~chart_data$nbourhood_count,
-            color = "orange"
-    ) %>%
-    layout(title = indicator_name,
-           xaxis = list(hoverformat = ".1f",
-                        title = "Area overall value & maximum & minimum"))
-  
-  chart_data %>%
-  plot_ly() %>%
-    add_trace(type = "box",
-              y = list("Bolton", "Neighbourhood"),
-              q1 = ~list(bolton_min, nbourhood_min),
-              # calculated value if it's available otherwise median
-              median = ~list(bolton_value,
-                            ifelse(is.na(nbourhood_pct), 
-                                   nbourhood_median,
-                                   nbourhood_pct)
-              ),
-              q3 = ~list(bolton_max, nbourhood_max),
-              notchspan = list(0.3, 0.3),
-             # hovertext = ~chart_data$nbourhood_count,
-              color = "orange"
-    ) %>%
-    layout(title = ~IndicatorName,
-           xaxis = list(hoverformat = ".1f",
-                        title = "Area overall value & maximum & minimum"))
-
 
 ###################### nomis ##########################
 
---
+++ visualise_data.R
@@ -0,0 +1,123 @@
+# map/ chart files
+
+library(dplyr)
+# library(shinycssloaders)
+# library(fingertipsR)
+library(data.table)
+#library(nomisr)
+#library(readxl)
+
+##################### map ###############################
+
+library(sf)
+library(leaflet)
+library(leaflet.extras)
+
+mytitle <- glue::glue("<b>Title</b><br>
+                      Subtitle | Turn layers on & off to explore")
+
+lsoa_labels <- (glue::glue("<b>LSOA</b><br>
+                    Code: {lsoas_bolton$lsoa11cd}<br>
+                    Name: {lsoas_bolton$lsoa11nmw} <br>
+                    All age population: {lsoas_bolton$all_age_pop}"))
+
+msoa_labels = (glue::glue("<b>MSOA</b><br>
+                    Code: {msoas_bolton$msoa11cd}<br>
+                    Name: {msoas_bolton$msoa11nmw}<br>
+                    House of Commons Library name: {msoas_bolton$local_name}<br>
+                    Covid cases rolling rate: {msoas_bolton$new_cases_by_specimen_date_rolling_rate}<br>
+                    Covid cases date: {format(msoas_bolton$date, '%d/%m/%Y')}"))
+
+# make colour palatte 
+msoa_colours <- colorNumeric(
+  palette = "Greens", 
+  domain = msoas_bolton$new_cases_by_specimen_date_rolling_rate, 
+  na.color = "white")
+
+leaflet() %>%
+  addResetMapButton() %>%
+  addProviderTiles("Stamen.TonerLite") %>%
+  addPolylines(data = neighbourhood_boundaries, weight = 4, color = "black") %>%
+  addPolygons(data = msoas_bolton, weight = 0.75, color = "grey", 
+              fillColor = ~msoa_cases_colours(new_cases_by_specimen_date_rolling_rate), fillOpacity = 0.5, group = "MSOA cases",
+              highlight = highlightOptions(weight = 4, color = "grey", bringToFront = TRUE),
+              popup = ~msoa_labels, 
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto"))
+
+###################### chart ##########################
+
+library(plotly)
+
+neighbourhood_indicators <- readRDS("./bolton_neighbourhoods/neighbourhood_indicators.RDS") # in the app folder
+
+neighbourhood_name <- "Turton"
+neighbourhood_name <- "Central/Great Lever"
+neighbourhood_name <- "Chorley Roads"
+
+indicator_name <- "Percentage of the total resident population who are 65 and over"
+
+chart_data <- neighbourhood_indicators %>%
+  st_drop_geometry() %>%
+  filter(neighbourhood == neighbourhood_name &   
+           IndicatorName == indicator_name) %>%
+  #group_by(neighbourhood) %>%
+  slice(1) 
+
+# chart
+plot_ly() %>%
+  add_trace(type = "box",
+            y =1,
+            q1 = chart_data$nbourhood_min,
+            # calculated value if it's available otherwise median
+            median = ifelse(is.na(chart_data$nbourhood_pct), chart_data$nbourhood_median,chart_data$nbourhood_pct),
+            q3 = chart_data$nbourhood_max,
+            lowerfence = chart_data$bolton_min,
+            upperfence = chart_data$bolton_max,
+            notchspan = 0.3
+  )
+
+# chart
+plot_ly() %>%
+  add_trace(type = "box",
+            y = list("Bolton", "Neighbourhood"),
+            q1 = list(chart_data$bolton_min, chart_data$nbourhood_min),
+            # calculated value if it's available otherwise median
+            median = list(chart_data$bolton_value,
+                          ifelse(is.na(chart_data$nbourhood_pct), 
+                                 chart_data$nbourhood_median,
+                                 chart_data$nbourhood_pct)
+            ),
+            q3 = list(chart_data$bolton_max, chart_data$nbourhood_max),
+            #min = list(chart_data$bolton_min, chart_data$nbourhood_min),
+            #  upperfence = chart_data$bolton_max,
+            notchspan = list(0.3, 0.3),
+            #hovertemplate = "some text {chart_data$bolton_max}",
+            hovertext = ~chart_data$nbourhood_count,
+            color = "orange"
+  ) %>%
+  layout(title = indicator_name,
+         xaxis = list(hoverformat = ".1f",
+                      title = "Area overall value & maximum & minimum"))
+
+chart_data %>%
+  plot_ly() %>%
+  add_trace(type = "box",
+            y = list("Bolton", "Neighbourhood"),
+            q1 = ~list(bolton_min, nbourhood_min),
+            # calculated value if it's available otherwise median
+            median = ~list(bolton_value,
+                           ifelse(is.na(nbourhood_pct), 
+                                  nbourhood_median,
+                                  nbourhood_pct)
+            ),
+            q3 = ~list(bolton_max, nbourhood_max),
+            notchspan = list(0.3, 0.3),
+            # hovertext = ~chart_data$nbourhood_count,
+            color = "orange"
+  ) %>%
+  layout(title = ~IndicatorName,
+         xaxis = list(hoverformat = ".1f",
+                      title = "Area overall value & maximum & minimum"))
