7b2fb9be9c1470ea024f51ad0d0b7b123e5f2459
Wilkinson <Shan.Wilkinson2@bolton.gov.uk>
2023-01-19 16:47:23 +0000

    sorted app with the extra tabs

--- fingertips local health.R
+++ fingertips local health.R
@@ -92,29 +92,60 @@ app_location <- "./bolton_neighbourhoods_app/"
         
 ####### transform to neighbourhood level ##############################################################
 
+    # msoa z score
+    msoa_standardised <- local_health_all_msoa %>%
+      filter(AreaType == "MSOA") %>%
+      # keep latest value only - only seems to include latest anyway
+      group_by(IndicatorId, Sex, Age) %>%
+      filter(TimePeriodSortable == max(TimePeriodSortable)) %>%
+      mutate(
+        msoa_z = (Value - mean(Value, na.rm = TRUE))/ sd(Value, na.rm = TRUE)
+      ) %>%
+      filter(stringr::str_detect(AreaName, "^Bolton"))  %>%
+    ungroup()        
+    
 # combine indicators but keep msoa level so can have 1 dataset
   
-  nbourhood_indicators <- bolton_local_health2 %>%
-    group_by(IndicatorId, Sex, Age, TimePeriodSortable, neighbourhood) %>%
-    mutate(nbourhood_count = sum(Count), 
-           nbourhood_denominator = sum(Denominator),
-           nbourhood_pct = nbourhood_count/ nbourhood_denominator*100,
-           nbourhood_median = median(Value),
-           nbourhood_max = max(Value, na.rm = TRUE),
-           nbourhood_min = min(Value, na.rm = TRUE),
-           nbourhood_q1 = quantile(Value, 0.25, na.rm = TRUE),
-           nbourhood_q3 = quantile(Value, 0.75, na.rm = TRUE)
-           ) %>%
-    ungroup() %>%
-    # bolton min & max
-    group_by(IndicatorId, Sex, Age, TimePeriodSortable) %>%
-    mutate(bolton_min = min(Value, na.rm = TRUE),
-           bolton_max = max(Value, na.rm = TRUE),
-           bolton_q1 = quantile(Value, 0.25, na.rm = TRUE),
-           bolton_median = median(Value, na.rm = TRUE),
-           bolton_q3 = quantile(Value, 0.75, na.rm = TRUE),
-           neighbourhood = ifelse(is.na(neighbourhood) & AreaName == "Bolton",
-            "Bolton", neighbourhood))
+    nbourhood_indicators <- bolton_local_health2 %>%
+      left_join(msoa_standardised %>%
+                  select(IndicatorId, Sex, Age, TimePeriodSortable, AreaCode, msoa_z), 
+                by = c("IndicatorId", "Sex", "Age", "TimePeriodSortable", "AreaCode")
+      ) %>%
+      group_by(IndicatorId, Sex, Age, TimePeriodSortable, neighbourhood) %>%
+      mutate(nbourhood_count = sum(Count), 
+             nbourhood_denominator = sum(Denominator),
+             nbourhood_pct = nbourhood_count/ nbourhood_denominator*100,
+             nbourhood_median = median(Value, na.rm = TRUE),
+             nbourhood_max = max(Value, na.rm = TRUE),
+             nbourhood_min = min(Value, na.rm = TRUE),
+             nbourhood_q1 = quantile(Value, 0.25, na.rm = TRUE),
+             nbourhood_q3 = quantile(Value, 0.75, na.rm = TRUE),
+             z_nbourhood_median = median(msoa_z, na.rm = TRUE),
+             z_nbourhood_max = max(msoa_z, na.rm = TRUE),
+             z_nbourhood_min = min(msoa_z, na.rm = TRUE),
+             z_nbourhood_q1 = quantile(msoa_z, 0.25, na.rm = TRUE),
+             z_nbourhood_q3 = quantile(msoa_z, 0.75, na.rm = TRUE),
+             z_nbourhoood_median_abs = abs(z_nbourhood_median),
+             z_nbourhood_iqr_abs = abs(z_nbourhood_q3 - z_nbourhood_q1),
+             z_nbourhood_range_abs = abs(z_nbourhood_max - z_nbourhood_min)
+      ) %>%
+      ungroup() %>%
+      # get direction of absolute values
+      mutate(
+        z_nbourhood_median_abs_direction = case_when(
+          z_nbourhood_median >1.96 ~ "much higher", # 95% of a normal distribution lie between +1.96 & -1.96
+          z_nbourhood_median >0 ~ "higher",
+          z_nbourhood_median <0 ~ "lower",
+          z_nbourhood_median <1.96 ~ "much lower",
+          z_nbourhood_median ==0 ~ "average")
+      ) %>%
+      # bolton min & max
+      group_by(IndicatorId, Sex, Age, TimePeriodSortable) %>%
+      mutate(bolton_min = min(Value, na.rm = TRUE),
+             bolton_max = max(Value, na.rm = TRUE),
+             bolton_q1 = quantile(Value, 0.25, na.rm = TRUE),
+             bolton_median = median(Value, na.rm = TRUE),
+             bolton_q3 = quantile(Value, 0.75, na.rm = TRUE))
     
   
   # pivot to get bolton value in a different column
@@ -131,52 +162,53 @@ app_location <- "./bolton_neighbourhoods_app/"
   )
   
   # get England values 
-  
-    # actual England figure
-    england_indicators <- local_health_all_msoa %>%
-      filter(AreaType == "England") %>%
-      # keep latest value only - only seems to include latest anyway
-      group_by(IndicatorId, Sex, Age) %>%
-      filter(TimePeriodSortable == max(TimePeriodSortable)) %>%
-      slice(1) %>% # each seems to be duplicated, keep 1st only
-      ungroup()
 
-    # england MSOA max/min
-    england_min_max <- local_health_all_msoa %>%
-      filter(AreaType == "MSOA") %>%
-      # keep latest value only - only seems to include latest anyway
-      group_by(IndicatorId, Sex, Age) %>%
-      filter(TimePeriodSortable == max(TimePeriodSortable)) %>%
-      mutate(
-        england_min = min(Value, na.rm = TRUE),
-        england_max = max(Value, na.rm = TRUE),
-        england_q1 = quantile(Value, 0.25, na.rm = TRUE),
-        england_median = median(Value, na.rm = TRUE),
-        england_q3 = quantile(Value, 0.75, na.rm = TRUE)) %>%
-      slice(1)
-    
-    # combined for joining
-    england_values <- full_join(
-      england_indicators %>%
-        select(IndicatorId, Sex, Age, TimePeriodSortable, Value)
-      ,
-      england_min_max %>%
-        select(IndicatorId, Sex, Age, TimePeriodSortable, england_min: england_q3)
-      ,
-      by = c("IndicatorId", "Sex", "Age", "TimePeriodSortable")
-    )
+  # actual England figure
+  england_indicators <- local_health_all_msoa %>%
+    filter(AreaType == "England") %>%
+    # keep latest value only - only seems to include latest anyway
+    group_by(IndicatorId, Sex, Age) %>%
+    filter(TimePeriodSortable == max(TimePeriodSortable)) %>%
+    ungroup()
   
-    # join in england
-    nbourhood_indicators2b <- left_join(nbourhood_indicators2, 
-                                        england_values %>%
-                                          rename("england_value"= "Value"),
-                                        by = c("IndicatorId", "Sex", "Age", "TimePeriodSortable"), 
-                                        suffix = c("", "_england")) %>%
-      # give new indicator name for sex disaggregated indicators
-      mutate(IndicatorName = ifelse(!Sex %in% c("Persons", "Not applicable"), 
-                                    paste(IndicatorName, Sex, sep = " - "),
-                                    IndicatorName)
-      )
+  # england MSOA max/min
+  england_min_max <- local_health_all_msoa %>%
+    filter(AreaType == "MSOA") %>%
+    # keep latest value only - only seems to include latest anyway
+    group_by(IndicatorId, Sex, Age) %>%
+    filter(TimePeriodSortable == max(TimePeriodSortable)) %>%
+    mutate(
+      england_min = min(Value, na.rm = TRUE),
+      england_max = max(Value, na.rm = TRUE),
+      england_q1 = quantile(Value, 0.25, na.rm = TRUE),
+      england_median = median(Value, na.rm = TRUE),
+      england_q3 = quantile(Value, 0.75, na.rm = TRUE)) %>%
+    slice(1)
+  
+  # combined for joining
+  england_values <- full_join(
+    england_indicators %>%
+      select(IndicatorId, Sex, Age, TimePeriodSortable, Value)
+    ,
+    england_min_max %>%
+      select(IndicatorId, Sex, Age, TimePeriodSortable, england_min: england_q3)
+    ,
+    by = c("IndicatorId", "Sex", "Age", "TimePeriodSortable")
+  )
+  
+  # join in england
+  nbourhood_indicators2b <- left_join(nbourhood_indicators2, 
+                                      england_values %>%
+                                        rename("england_value"= "Value"),
+                                      by = c("IndicatorId", "Sex", "Age", "TimePeriodSortable"), 
+                                      suffix = c("", "_england")) %>%
+    # move absolute median z direction as it's not numeric so mutate across to 1 decimal place still works
+    relocate(z_nbourhood_median_abs_direction, .after = england_q3) %>%
+    # give new indicator name for sex disaggregated indicators
+    mutate(IndicatorName = ifelse(!Sex %in% c("Persons", "Not applicable"), 
+                                  paste(IndicatorName, Sex, sep = " - "),
+                                  IndicatorName)
+    )
   
   # get msoa boundaries    
     msoa_boundaries <- readRDS("msoa boundaries.RDS")
