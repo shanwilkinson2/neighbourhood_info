3568830696384d87e2b218a11e4e4c4a1b7dce6c
Wilkinson <Shan.Wilkinson2@bolton.gov.uk>
2023-02-22 12:53:42 +0000

    age from census lsoa

--- lsoa level data.R
+++ lsoa level data.R
@@ -17,31 +17,23 @@ lsoa_neighbourhood <- read.xlsx("6 neighbourhoods final option.xlsx") %>%
   rename(imd_rank = index_of_multiple_deprivation_imd_rank, 
            imd_decile = index_of_multiple_deprivation_imd_decile) %>%
     pivot_longer(cols = -c(lsoa_code_2011:local_authority_district_name_2019),
-                 values_to = "Value", names_to = "indicator")
+                 values_to = "Value", names_to = "IndicatorName")
   
   # add in neighbourhood
   lsoa_standardised <- imd_overall %>%
     full_join(lsoa_neighbourhood,
                                  by = c("lsoa_code_2011" = "lsoa_name")) %>%
-    group_by(indicator) %>%
+    group_by(IndicatorName) %>%
     mutate(
       lsoa_z = (Value - mean(Value, na.rm = TRUE))/ sd(Value, na.rm = TRUE)
     ) %>%
     filter(local_authority_district_name_2019 == "Bolton")  %>%
-    ungroup() 
+    ungroup() %>%
+    rename(neighbourhood = x6_areas_name)
   
-  ##### REACHED HERE #################
-  
-  nbourhood_lsoa <- lsoa_standardised %>%
-    left_join(imd_overall %>%
-                filter(local_authority_district_name_2019 == "Bolton") %>%
-                select(lsoa_code_2011, indicator, Value) , 
-              by = c("indicator", "lsoa_code_2011")
-    ) %>%
-    
-
-    
-    group_by(indicator, neighbourhood) %>%
+ 
+  nbourhood_indicators <- lsoa_standardised %>%
+    group_by(IndicatorName, neighbourhood) %>%
     mutate(nbourhood_count = NA, 
            nbourhood_denominator = NA,
            nbourhood_pct = NA,
@@ -50,7 +42,7 @@ lsoa_neighbourhood <- read.xlsx("6 neighbourhoods final option.xlsx") %>%
            nbourhood_min = min(Value, na.rm = TRUE),
            nbourhood_q1 = quantile(Value, 0.25, na.rm = TRUE),
            nbourhood_q3 = quantile(Value, 0.75, na.rm = TRUE),
-           z_nbourhood_median = median(msoa_z, na.rm = TRUE),
+           z_nbourhood_median = median(lsoa_z, na.rm = TRUE),
            z_nbourhood_max = max(lsoa_z, na.rm = TRUE),
            z_nbourhood_min = min(lsoa_z, na.rm = TRUE),
            z_nbourhood_q1 = quantile(lsoa_z, 0.25, na.rm = TRUE),
@@ -70,27 +62,43 @@ lsoa_neighbourhood <- read.xlsx("6 neighbourhoods final option.xlsx") %>%
         z_nbourhood_median ==0 ~ "average")
     ) %>%
     # bolton min & max
-    group_by(IndicatorId, Sex, Age, TimePeriodSortable) %>%
+    group_by(IndicatorName) %>%
     mutate(bolton_min = min(Value, na.rm = TRUE),
            bolton_max = max(Value, na.rm = TRUE),
            bolton_q1 = quantile(Value, 0.25, na.rm = TRUE),
            bolton_median = median(Value, na.rm = TRUE),
            bolton_q3 = quantile(Value, 0.75, na.rm = TRUE))
   
+ for_julie <- nbourhood_indicators %>%
+   select(neighbourhood, IndicatorName, nbourhood_median:ncol(.)) %>%
+   group_by(neighbourhood, IndicatorName) %>%
+   slice(1)
+   
+ for_julie2 <- readRDS("./bolton_neighbourhoods/neighbourhood_indicators.RDS") %>%
+   sf::st_drop_geometry() %>%
+   filter(IndicatorId == 93283) %>%
+   select(neighbourhood, IndicatorName, nbourhood_median:ncol(.)) %>%
+   group_by(IndicatorName, neighbourhood) %>%
+   slice(1)
+ 
+ for_julie_all <- bind_rows(for_julie, for_julie2)
+ 
+ write.xlsx(for_julie_all, "C:/temp/neighbourhood_data.xlsx")
   
-  # pivot to get bolton value in a different column
-  nbourhood_indicators2 <- left_join(
-    nbourhood_indicators %>%
-      ungroup() %>%
-      filter(neighbourhood != "Bolton"),
-    nbourhood_indicators %>%
-      ungroup() %>%
-      filter(neighbourhood == "Bolton") %>%
-      select(IndicatorId, Sex, Age, TimePeriodSortable, bolton_value = nbourhood_median), # median will be the value as all bolton
-    by = c("IndicatorId", "Sex", "Age", "TimePeriodSortable"),
-    suffix = c("_neighbourhood", "_bolton")
-  )
-  
+  # no england value
+  # # pivot to get bolton value in a different column
+  # nbourhood_indicators2 <- left_join(
+  #   nbourhood_indicators %>%
+  #     ungroup() %>%
+  #     filter(neighbourhood != "Bolton"),
+  #   nbourhood_indicators %>%
+  #     ungroup() %>%
+  #     filter(neighbourhood == "Bolton") %>%
+  #     select(bolton_value = nbourhood_median), # median will be the value as all bolton
+  #   by = c("Indicator"),
+  #   suffix = c("_neighbourhood", "_bolton")
+  # )
+  # 
   # get England values 
   
   # actual England figure
@@ -139,4 +147,89 @@ lsoa_neighbourhood <- read.xlsx("6 neighbourhoods final option.xlsx") %>%
                                   paste(IndicatorName, Sex, sep = " - "),
                                   IndicatorName)
     )
+  
+  # census age info - 5 year bands only at lsoa
+  
+  age <- read.csv("census2021-ts007a-lsoa.csv") %>%
+    janitor::clean_names() %>%
+    pivot_longer(cols = -c(date:age_total),
+                 values_to = "Value",
+                 names_to = "IndicatorName") %>%
+    rename(num = Value) %>%
+    mutate(Value = num/age_total *100)
+  
+  # add in neighbourhood
+  lsoa_standardised <- age %>%
+    full_join(lsoa_neighbourhood,
+              by = c("geography_code" = "lsoa_name")) %>%
+    group_by(IndicatorName) %>%
+    mutate(
+      lsoa_z = (Value - mean(Value, na.rm = TRUE))/ sd(Value, na.rm = TRUE)
+    ) %>%
+    filter(stringr::str_detect(geography, "^Bolton"))  %>%
+    ungroup() %>%
+    rename(neighbourhood = x6_areas_name)
+  
+  
+  neighbourhood_pop <- lsoa_standardised %>%
+    select(geography, neighbourhood, age_total) %>%
+    group_by(geography, neighbourhood) %>%
+    # get only 1 age total row per lsoa
+    slice(1) %>%
+    ungroup() %>%
+    group_by(neighbourhood) %>%
+    summarise(nbourhood_denominator = sum(age_total))
+    
+  nbourhood_indicators <- lsoa_standardised %>%
+    group_by(IndicatorName, neighbourhood) %>%
+    mutate(nbourhood_count = NA, 
+           nbourhood_denominator = NA,
+           nbourhood_pct = NA,
+           nbourhood_median = median(Value, na.rm = TRUE),
+           nbourhood_max = max(Value, na.rm = TRUE),
+           nbourhood_min = min(Value, na.rm = TRUE),
+           nbourhood_q1 = quantile(Value, 0.25, na.rm = TRUE),
+           nbourhood_q3 = quantile(Value, 0.75, na.rm = TRUE),
+           z_nbourhood_median = median(lsoa_z, na.rm = TRUE),
+           z_nbourhood_max = max(lsoa_z, na.rm = TRUE),
+           z_nbourhood_min = min(lsoa_z, na.rm = TRUE),
+           z_nbourhood_q1 = quantile(lsoa_z, 0.25, na.rm = TRUE),
+           z_nbourhood_q3 = quantile(lsoa_z, 0.75, na.rm = TRUE),
+           z_nbourhoood_median_abs = abs(z_nbourhood_median),
+           z_nbourhood_iqr_abs = abs(z_nbourhood_q3 - z_nbourhood_q1),
+           z_nbourhood_range_abs = abs(z_nbourhood_max - z_nbourhood_min)
+    ) %>%
+    ungroup() %>%
+    # get direction of absolute values
+    mutate(
+      z_nbourhood_median_abs_direction = case_when(
+        z_nbourhood_median >1.96 ~ "much higher", # 95% of a normal distribution lie between +1.96 & -1.96
+        z_nbourhood_median >0 ~ "higher",
+        z_nbourhood_median <0 ~ "lower",
+        z_nbourhood_median <1.96 ~ "much lower",
+        z_nbourhood_median ==0 ~ "average")
+    ) %>%
+    # bolton min & max
+    group_by(IndicatorName) %>%
+    mutate(bolton_min = min(Value, na.rm = TRUE),
+           bolton_max = max(Value, na.rm = TRUE),
+           bolton_q1 = quantile(Value, 0.25, na.rm = TRUE),
+           bolton_median = median(Value, na.rm = TRUE),
+           bolton_q3 = quantile(Value, 0.75, na.rm = TRUE)
+    ) %>%
+    ungroup() %>%
+    group_by(IndicatorName, neighbourhood) %>%
+    select(-nbourhood_denominator) %>%
+    left_join(neighbourhood_pop, by = ("neighbourhood")) %>%
+    relocate(nbourhood_denominator, .after = nbourhood_count) %>%
+    mutate(nbourhood_count = sum(num),
+           nbourhood_pct = nbourhood_count/nbourhood_denominator*100
+           )
+
+  
+  for_julie3 <- nbourhood_indicators %>%
+    group_by(neighbourhood, IndicatorName) %>%
+    slice(1) %>%
+    select(neighbourhood, IndicatorName, nbourhood_count:ncol(.))
+  write.xlsx(for_julie3, "C:/temp/neighbourhood_age.xlsx")
   
